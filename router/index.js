481ad87c9c412674089cfc6082f358beca901fd5
Muhammad Ajie Rizaldi <ajiemuhammad2@gmail.com>
2022-10-26 03:01:39 +0700

    update index.ejs, partials, and public route

--- router/index.js
+++ router/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
-// const userRoute = require('./userRoute');
-const medicineRoute = require('./medicineRoute');
-// const patientRoute = require('./patientRoute');
 
+// route obat
+const medicineRoute = require('./medicineRoute');
+// route pasien
+// const patientRoute = require('./patientRoute');
+// // route admin
+// const userRoute = require('./userRoute');
+
+// main dashboard
 router.get('/', (req, res) => {
     res.render('index');
+});
+
+// route login
+router.get('/login', (req, res) => {
+    res.render('pages/login');
 });
 
 // test api
@@ -13,7 +23,8 @@ router.get('/check-health', (req, res) => {
     res.send('Server is running');
 })
 router.use('/medicine', medicineRoute);
-
+// router.use('/patient', patientRoute);
+// router.use('/user', userRoute);
 
 
 module.exports = router;
