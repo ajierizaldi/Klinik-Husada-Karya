9dddc0551ae6b601ed3a6af3f9dffa2b223afa02
Muhammad Ajie Rizaldi <ajiemuhammad2@gmail.com>
2022-10-27 23:30:57 +0700

    update index.ejs, partials(footer,navbar, sidebar) and styling css external

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,1 +1,8 @@
 const model = require('../models');
+
+module.exports = {
+    list: async (req, res) => { },
+    create: async (req, res) => { },
+    update: async (req, res) => { },
+    destroy: async (req, res) => { }
+}
