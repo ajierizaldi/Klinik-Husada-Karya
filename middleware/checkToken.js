4ae9fc1be10a58fdccef7b33ecb0e7fb17f92cb1
Muhammad Ajie Rizaldi <ajiemuhammad2@gmail.com>
2022-10-29 16:58:16 +0700

    update middleware

--- middleware/checkToken.js
+++ middleware/checkToken.js
@@ -11,6 +11,34 @@ const checkToken = (req, res, next) => {
             data: null,
         });
     }
+
+    if (token.toLowerCase().startWith("bearer")) {
+        token = token.slice("bearer".length).trim();
+    }
+
+    try {
+        const jwtPayLoad = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+        if (!jwtPayLoad) {
+            return res.status(403).json({
+                success: false,
+                error: 403,
+                message: "Invalid token",
+                data: null
+            });
+        }
+
+        res.locale.user = jwtPayLoad;
+
+        next();
+    } catch (error) {
+        return res.status(403).json({
+            success: false,
+            error: 403,
+            message: "Invalid token",
+            data: null,
+        });
+    }
 }
 
-module.exports = checkToken
+module.exports = checkToken;
